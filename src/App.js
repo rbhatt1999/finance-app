17555456e6cdabf6806089e0bcd748fcfde4dc53
rbhatt199924 <rbhatt199924@gmail.com>
2023-02-05 18:37:47 +0530

    Setup App and index file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Dashboard from './Components/Pages/Dashboard';
+import Details from './Components/Pages/Details';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route
+      path="/"
+      element={(
+        <Container fluid className="App">
+          <Dashboard />
+          {' '}
+        </Container>
+      )}
+    />
+    <Route
+      path="/details"
+      element={(
+        <Container fluid className="App">
+          <Details />
+        </Container>
+      )}
+    />
+  </Routes>
+    </BrowserRouter>
   );
 }
 
